79c30d5aaa3a8ef8ae09637f72614622147e9a9c
Janaina Ludwig <janainaludwig@alunos.utfpr.edu.br>
2019-09-26 22:07:16 -0300

    Hamburguer bug fixed

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -5,43 +5,12 @@ import Content from './content';
 import '../styles/content.scss';
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        isSidebarToggled: false,
-    }
-    this.fetchToggle = this.fetchToggle.bind(this);
-  }
-
-  fetchToggle() {
-
-    new Promise((resolve, reject) => {
-        let app = document.querySelector("#app");
-
-        if (this.state.isSidebarToggled) {
-            app.classList.add('sidebar-hidden')
-        } else {
-            app.classList.remove('sidebar-hidden')
-
-        }
-        resolve();
-    })
-        .then(() => {
-            this.setState({
-                isSidebarToggled: !this.state.isSidebarToggled
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }
-
     render() {
         return(
           <div id="root"> 
             <Sidebar/>
             <div id="app-content">
-                <Stickybar fetchToggle={this.fetchToggle} />
+                <Stickybar/>
                 
                 <div className="content-container">
                   <Content/>

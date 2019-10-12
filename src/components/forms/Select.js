ef81d76efd885ee57846af3780ddc62bb0cf13fe
Janaina Ludwig <janainaludwig@alunos.utfpr.edu.br>
2019-10-12 01:29:10 -0300

    Select component

--- src/components/forms/Select.js
+++ src/components/forms/Select.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function ResgisterModal(props) {
-  // return (
-    
-  // )
+function Option({value, description}) {
+  return (<option key={value} value={value}>{description}</option>);
 }
 
-export default ResgisterModal;
+export function Select({options, valueAttr = 'id', descriptionAttr = 'name', label, selected, ...rest}) {
+  return (
+    <div className="form-group">
+      <select required={true} defaultValue={selected || 0} {...rest} className="form-control">
+        <option value="0" disabled>Selecione...</option>
+        {options.map(
+          item => (
+            <Option value={item[valueAttr]} description={item[descriptionAttr]} />
+          )
+        )}
+      </select>
+      <label>{label}</label>
+    </div>
+  )
+}

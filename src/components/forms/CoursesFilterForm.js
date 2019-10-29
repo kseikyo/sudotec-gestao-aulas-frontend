d3ce0285da47b9dfeeeadc7dd54c01c88852d4a3
SergioTrentinJr <sergiotrentinjr@gmail.com>
2019-10-29 20:49:27 -0300

    added CoursersFilterForm

---
+++ src/components/forms/CoursesFilterForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import SearchInput from './SearchInput';
+import Select from './Select';
+import {changeHandler} from './handler';
+
+export default class CoursesFilterForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            projects: props.projects,
+            courses: props.courses,
+            formControls: {
+                project_id: null,
+                search: "",
+                course_status: null
+            },
+        };
+        this.changeHandler  = changeHandler.bind(this);
+    }
+    
+    render() {
+        return(
+            <div className="d-inline-flex">
+                <Select label="Projetos" name="project_id" value={this.state.formControls.project_id} onInput={(event) => {this.changeHandler(event)}}  options={this.state.projects} defaultValue="Todos"/>
+                <Select 
+                    label='Status' name='course_status' 
+                    value={this.state.formControls.course_status} 
+                    onInput={this.changeHandler} options={this.state.courses.status} 
+                />
+                <SearchInput name="search" onChange={this.changeHandler}/>
+            </div>
+        );
+    }
+}
